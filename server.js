e5fb286d4c03a6af43015450fe89209d6b4ed787
Ubuntu <ubuntu@ip-172-31-43-127.eu-north-1.compute.internal>
2025-09-18 09:53:41 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'dev';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(`Hello from ${env} environment!\n`);
+});
+
+server.listen(port, () => console.log(`Server running on ${port}`));
+
